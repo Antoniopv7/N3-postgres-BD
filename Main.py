e2664ef9b59294950803be20602ee11c3de55c79
Antoniopv7 <71039689+Antoniopv7@users.noreply.github.com>
2022-12-11 20:54:09 -0300

    cambios

--- Main.py
+++ Main.py
@@ -1,5 +1,15 @@
 import psycopg2
-import Libreria as li
+
+#Conexion a la BD
+connection=psycopg2.connect(
+        host='localhost',
+        user='postgres',
+        password='Antonivan0',
+        database='BDproyect'
+    )
+
+# Utilizacion del cursor
+cursor=connection.cursor()
 
 def pedirNumeroEntero():
     correcto=False
@@ -51,7 +61,38 @@ while not salir:
             x = pedirNumeroEntero()
 
             if x==1:
-                print("")
+                #sentencia
+                sql = 'INSERT INTO cliente (cliente_id,nombre,apellido,rut,direccion,telefono) VALUES(%s,%s,%s,%s,%s,%s)'
+                #solicitud de datos
+                correcto=False
+                cliente_id = 0
+                while(not correcto):
+                    try:
+                        cliente_id=int(input("Ingrese el id del cliente: "))
+                        correcto=True
+                    except ValueError:
+                        print("Error, introduzca un numero entero")
+                nombre=input("Ingrese el nombre del cliente: ")
+                apellido=input("Ingrese el apellido del cliente: ")
+                rut=input("Ingrese el rut del cliente: ")
+                direccion=input("Ingrese la direccion del cliente: ")
+                a=False
+                telefono = 0
+                while(not a):
+                    try:
+                        telefono=int(input("Ingrese el numero telefonico del cliente: "))
+                        a=True
+                    except ValueError:
+                        print("Error, introduzca un numero entero")
+                #recoleccion de datos
+                datos=(cliente_id,nombre,apellido,rut,direccion,telefono)
+                #hacer uso del metodo execute
+                cursor.execute(sql,datos)
+                #guardar registro
+                connection.commit()
+                #registros insertados
+                print('Datos insertados')
+
             elif x==2:
                 print("")
             elif x==3:
@@ -97,12 +138,20 @@ while not salir:
             if x == 1:
                 lista=["Id","Nombre","Apellido","Rut","Direccion","Telefono"]
                 print(lista)
-                li.listado_de_clientes()
+                sql='SELECT * FROM cliente'
+                cursor.execute(sql)
+                registro=cursor.fetchall()
+                for fila in registro:
+                    print(fila)
                 print("")
             elif x == 2:
                 lista=["Id","Nombre","Apellido","Rut","Sueldo"]
-                print("lista")
-                li.listado_despachadores()
+                print(lista)
+                sql='SELECT * FROM empleado'
+                cursor.execute(sql)
+                registro=cursor.fetchall()
+                for fila in registro:
+                    print(fila)
                 print("")
             elif x == 3:
                 print("")
@@ -215,5 +264,7 @@ while not salir:
     else:
         print ("Seleccione una de las opciones")
 
+cursor.close()
+connection.close()
 print ("")
 print ("Usted esta saliendo de la Base de datos")

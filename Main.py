04c19a8636fbb6738fb72a3f2c0c109b9e2fa69c
Antoniopv7 <71039689+Antoniopv7@users.noreply.github.com>
2022-12-14 22:47:20 -0300

    cambios

--- Main.py
+++ Main.py
@@ -44,6 +44,21 @@ def Guardar_nuevo_empleado(nombre,apellido,rut,sueldo):
     "Datos Insertados")
     conn.close()
 
+def Guardar_nuevo_producto(nombre,descripcion,valor,stock):
+    conn = psycopg2.connect(
+        host='localhost',
+        user='postgres',
+        password='Antonivan0',
+        database='necu_db'
+    )
+    cursor = conn.cursor()
+    query = 'INSERT INTO producto (nombre,descripcion,valor,stock) VALUES(%s,%s,%s,%s)'
+    cursor.execute(query, (nombre,descripcion,valor,stock))
+    conn.commit()
+    showinfo("Necu BD",
+    "Datos Insertados")
+    conn.close()
+
 def mostrar_clientes(v3):
     conn=psycopg2.connect(
         host='localhost',
@@ -179,13 +194,13 @@ def ventana4():
     entry_sue = Entry(v4)
     entry_sue.grid(row=4,column=1)
 
-    boton_guardar_cliente = Button(v4,text="Guardar Empleado",command=lambda:Guardar_nuevo_empleado(
+    boton_guardar_empleado = Button(v4,text="Guardar Empleado",command=lambda:Guardar_nuevo_empleado(
         entry_nom.get(),
         entry_ape.get(),
         entry_rut.get(),
         entry_sue.get()
         ))
-    boton_guardar_cliente.grid(row=5,column=1)
+    boton_guardar_empleado.grid(row=5,column=1)
 
 def ventana5():
     v5 = Toplevel()
@@ -193,6 +208,37 @@ def ventana5():
     v5.title("Lista de clientes")
     labelv5 = Label(v5,text="| Id | Nombre | Apellido | Rut | Sueldo |").grid(row=0,column=0)
     mostrar_empleados(v5)
+
+def ventana6():
+    v6 = Toplevel()
+    v6.geometry("300x200")
+    v6.title("Insertar Datos")
+    espacio1=Label(v6,text="Producto").grid(row=0,column=0)
+    label1 = Label(v6,text="Ingrese Nombre: ").grid(row=1,column=0)
+    label2 = Label(v6,text="Ingrese Descripcion: ").grid(row=2,column=0)
+    label3 = Label(v6,text="Ingrese Valor: ").grid(row=3,column=0)
+    label4 = Label(v6,text="Ingrese Stock: ").grid(row=4,column=0)
+    espacio2 = Label(v6).grid(row=6,column=0)
+    espacio3 = Label(v6).grid(row=7,column=0)
+    espacio4 = Label(v6).grid(row=0,column=1)
+    espacio5 = Label(v6).grid(row=6,column=1)
+    espacio6 = Label(v6).grid(row=7,column=1)
+    entry_nom = Entry(v6)
+    entry_nom.grid(row=1,column=1)
+    entry_des = Entry(v6)
+    entry_des.grid(row=2,column=1)
+    entry_val = Entry(v6)
+    entry_val.grid(row=3,column=1)
+    entry_sto = Entry(v6)
+    entry_sto.grid(row=4,column=1)
+
+    boton_guardar_producto = Button(v6,text="Guardar Producto",command=lambda:Guardar_nuevo_producto(
+        entry_nom.get(),
+        entry_des.get(),
+        entry_val.get(),
+        entry_sto.get()
+        ))
+    boton_guardar_producto.grid(row=5,column=1)
 
 #Ventanas
 
@@ -203,6 +249,11 @@ espacio1_2 = Label(frame).grid(row=0,column=2)
 espacio2 = Label(frame).grid(row=1,column=1)
 espacio2_2 = Label(frame).grid(row=1,column=2)
 empleado = Label(frame,text="Empleado").grid(row=0,column=3)
+espacio3 = Label(frame).grid(row=0,column=4)
+espacio3_2 = Label(frame).grid(row=0,column=5)
+espacio4 = Label(frame).grid(row=1,column=4)
+espacio4_2 = Label(frame).grid(row=1,column=5)
+producto = Label(frame,text="Producto").grid(row=0,column=6)
 #label frame
 
 #button frame
@@ -210,6 +261,8 @@ boton_v2 = Button(frame, text="Insertar cliente",command=lambda:ventana2()).grid
 boton_v3 = Button(frame, text="Mostrar clientes",command=lambda:ventana3()).grid(row=2,column=0)
 boton_v4 = Button(frame, text="Insertar empleado",command=lambda:ventana4()).grid(row=1,column=3)
 boton_v5 = Button(frame, text="Mostrar empleados",command=lambda:ventana5()).grid(row=2,column=3)
+boton_v6 = Button(frame, text="Insertar Producto",command=lambda:ventana6()).grid(row=1,column=6)
+boton_v7 = Button(frame, text="Mostrar")
 #button frame
 
 root.mainloop()
